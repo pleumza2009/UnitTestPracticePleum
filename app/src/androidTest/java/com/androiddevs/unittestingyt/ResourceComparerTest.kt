8f334b9fb717646333ebcf846bee5a609b184abb
Thanakorn  Jaroensetthakul <35680283+pleumza2009@users.noreply.github.com>
2021-01-31 22:51:17 +0700

    Finish Basic UnitTest Let go  to Next Part

---
+++ app/src/androidTest/java/com/androiddevs/unittestingyt/ResourceComparerTest.kt
@@ -0,0 +1,39 @@
+package com.androiddevs.unittestingyt
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Test
+
+class ResourceComparerTest{
+
+    private lateinit var resourceComparer : ResourceComparer
+
+    @Before
+    fun setup(){
+        resourceComparer = ResourceComparer()
+    }
+
+    @After
+    fun teardown(){
+
+    }
+
+    @Test
+    fun stringResourceSameAsGivenString_returnTrue(){
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context,R.string.app_name,"UnitTestingYT")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringResourceDifferentAsGivenString_returnFalse(){
+
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context,R.string.app_name,"Hello")
+        assertThat(result).isFalse()
+    }
+}
---
+++ app/src/main/java/com/androiddevs/unittestingyt/ResourceComparer.kt
@@ -0,0 +1,11 @@
+package com.androiddevs.unittestingyt
+
+import android.content.Context
+
+class ResourceComparer {
+
+    fun isEqual(context:Context,resId:Int,string:String):Boolean{
+        return context.getString(resId) == string
+
+    }
+}
